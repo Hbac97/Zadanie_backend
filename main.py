3b5ddaa6b3b2d4ab3d386dab9f57c19caff98df8
Hbac97 <hubert.baciocha@gmail.com>
2021-11-11 19:11:38 +0100

    On branch master  Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,36 +1,70 @@
 from bs4 import BeautifulSoup as bs
 import requests 
+import psycopg2
 
-site = requests.get("https://teonite.com/blog/")
-src = site.content
-content = bs(src,'lxml')
+def scraper():
+    site = requests.get("https://teonite.com/blog/")
+    src = site.content
+    content = bs(src,'lxml')
 
-list_links=[]
-list_authors=[]
-list_content=[]
-list_posts=[]
-for articles in content.find_all(class_="post-card"):
-    a = articles.find('a')
-    author = articles.find(class_="author")
-    link = a.attrs['href']
-    list_authors.append(author.text)
-    list_links.append(link)
+    list_links=[]
+    list_authors=[]
+    list_content=[]
+    list_posts=[]
+    for articles in content.find_all(class_="post-card"):
+        a = articles.find('a')
+        author = articles.find(class_="author")
+        link = a.attrs['href']
+        list_authors.append(author.text)
+        list_links.append(link)
 
-    site2 = requests.get(f"https://teonite.com/{link}")
-    src2 = site2.content
-    content2 = bs(src2,'lxml')
+        site2 = requests.get(f"https://teonite.com/{link}")
+        src2 = site2.content
+        content2 = bs(src2,'lxml')
 
-    for post in content2.find_all("article"):
-        p0 = post.find('h3')
-        p1 = post.find_all('p')
-        for p in p0:
-            list_content.append(p.text)
-        for p in p1:
-            list_content.append(p.text)
-        list_posts[len(list_posts):] = [''.join(list_content[:])]
-        list_content=[]
+        for post in content2.find_all("article"):
+            p0 = post.find('h3')
+            p1 = post.find_all('p')
+            for p in p0:
+                list_content.append(p.text)
+            for p in p1:
+                list_content.append(p.text)
+            list_posts[len(list_posts):] = [''.join(list_content[:])]
+            list_content=[]
+    
 
-print(list_authors)
-print(*list_posts, sep='\n',end='\n')
-print(len(list_authors))
-print(len(list_posts))
+    print(list_authors)
+    print(*list_posts, sep='\n',end='\n')
+    print(len(list_authors))
+    print(len(list_posts))
+    list_combined = list(zip(list_authors,list_posts))
+    sql(list_combined)
+
+def sql(list_combined):
+    try: 
+        conn= psycopg2.connect(
+            user="postgres",
+            password="test123",
+            host="localhost",
+            port="5432",
+            database="database")
+        curs=conn.cursor()
+
+        postgres_insert= """INSERT INTO posts (AUTHORS,CONTENT) VALUES (%s,%s);"""
+        record_to_insert = (list_combined)
+        curs.executemany(postgres_insert,record_to_insert)
+
+        conn.commit()
+        count = curs.rowcount
+        print(count, "Authors Inserted successfully")
+
+    except(Exception, psycopg2.Error) as err:
+        print("Failed to insert",err)
+
+    finally:
+        if conn:
+            curs.close()
+            conn.close()
+            print("connection closed")
+
+scraper()
