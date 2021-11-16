fcb2fd2a2d70b3b0eb8660a568036cc658a28737
Hbac97 <91096189+Hbac97@users.noreply.github.com>
2021-11-16 21:52:55 +0100

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,22 +1,27 @@
 from bs4 import BeautifulSoup as bs
 import requests 
 import psycopg2
+import time
 
 def scraper():
     site = requests.get("https://teonite.com/blog/")
     src = site.content
     content = bs(src,'lxml')
 
-    list_links=[]
+    list_titles=[]
     list_authors=[]
     list_content=[]
     list_posts=[]
-    for articles in content.find_all(class_="post-card"):
+
+    content_posts = content.find(class_="post-cards")
+
+    for articles in content_posts.find_all(class_="post-card"):
         a = articles.find('a')
         author = articles.find(class_="author")
         link = a.attrs['href']
+        title = articles.find(class_="title")
         list_authors.append(author.text)
-        list_links.append(link)
+        list_titles.append(title.text)
 
         site2 = requests.get(f"https://teonite.com/{link}")
         src2 = site2.content
@@ -32,9 +37,10 @@ def scraper():
             list_posts[len(list_posts):] = [''.join(list_content[:])]
             list_content=[]
 
+    print(len(list_titles))
     print(len(list_authors))
     print(len(list_posts))
-    list_combined = list(zip(list_authors,list_posts))
+    list_combined = list(zip(list_authors,list_titles,list_posts))
     sql(list_combined)
 
 def sql(list_combined):
@@ -51,18 +57,21 @@ def sql(list_combined):
         curs.execute("""SELECT EXISTS(SELECT * FROM posts WHERE authors LIKE '%Gryczka%' LIMIT 1);""")
         if not curs.fetchone()[0]:
 
-            postgres_insert= """INSERT INTO posts (AUTHORS,CONTENT) VALUES (%s,%s);"""
+            postgres_insert= """INSERT INTO posts (AUTHORS,TITLES,CONTENT) VALUES (%s,%s,%s);"""
             record_to_insert = (list_combined)
             curs.executemany(postgres_insert,record_to_insert)
 
             conn.commit()
             count = curs.rowcount
-            print(count, "Authors and Content inserted successfully")
+            print(count, "Authors, Titles and Content inserted successfully")
         else:
             print ("Rows already exist")
 
     except(Exception, psycopg2.Error) as err:
         print("Failed to insert",err)
+        print("Trying again in 5 seconds...")
+        time.sleep(5)
+        scraper()
 
     finally:
         if conn:
